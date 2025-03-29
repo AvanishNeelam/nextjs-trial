ada17abf4bc4599445ac9ac3f44eb6de64a39c41
AvanishNeelam <neelamavanish41@gmail.com>
2025-03-29 12:14:41 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import ProductList from "../components/productList";
+import SearchBar from "../components/searchBar";
+export default function Home() {
+  return (
+    <>
+      <SearchBar />
+      <ProductList />
+    </>
+  );
+}
